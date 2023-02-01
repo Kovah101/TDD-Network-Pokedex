e14c26cf07b6635353969df4325ad4b894cea161
Elliot Thompson <elliotthompson101@gmail.com>
2023-02-01 15:03:24 +0000

    addition of local pokemon database

--- app/src/main/java/com/example/tddnetworkpokedex/core/models/Pokemon.kt
+++ app/src/main/java/com/example/tddnetworkpokedex/core/models/Pokemon.kt
@@ -2,6 +2,7 @@ package com.example.tddnetworkpokedex.core.models
 
 
 data class Pokemon(
+    val id: Int,
     val name: String,
     val url: String
 )
---
+++ app/src/main/java/com/example/tddnetworkpokedex/database/PokemonDatabase.kt
@@ -0,0 +1,10 @@
+package com.example.tddnetworkpokedex.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Pokemon::class], version = 1)
+abstract class PokemonDatabase : RoomDatabase() {
+
+    abstract fun pokemonDao(): PokemonDAO
+}
